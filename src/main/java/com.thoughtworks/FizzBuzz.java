cf21c170ac1b17f84f7290be2e049296816427d0
chenze.li <chenze.li@thoughtworks.com>
2020-01-12 11:52:50 +0800

    [Zachary]#FizzBuzz 'say fizz when times 5 or contains 5'

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -4,13 +4,20 @@ package com.thoughtworks;
 public class FizzBuzz {
 
   public String say(Integer number) {
-    if (number % 3 == 0 || isContainThree(number)) {
+    if ((number % 3 == 0 || isContainThree(number))&&!isContainFive(number)) {
       return "Fizz";
+    }
+    if (number % 5 == 0 || isContainFive(number)) {
+      return "Buzz";
     }
     return String.valueOf(number);
   }
 
   private boolean isContainThree(Integer number) {
     return String.valueOf(number).contains("3");
+  }
+
+  private boolean isContainFive(Integer number) {
+    return String.valueOf(number).contains("5");
   }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.groovy
+++ src/test/java/com/thoughtworks/FizzBuzzTest.groovy
@@ -4,14 +4,24 @@ class FizzBuzzTest extends spock.lang.Specification {
 
   def fizzBuzz = new FizzBuzz()
 
-  def 'say fizz when times 3 or contains 3'() {
+  def 'say Fizz when times 3 or contains 3'() {
     expect:
       fizzBuzz.say(number) == result
-
     where:
-      number || result
-      3      || "Fizz"
-      13     || "Fizz"
-      1      || "1"
+      number | result | description
+      3      | "Fizz" | "times 3"
+      13     | "Fizz" | "contain 3"
+      1      | "1"    | "other"
+      53     | "Buzz" | "contain 5"
   }
+
+  def 'say Buzz when times 5 or contains 5'() {
+    expect:
+      fizzBuzz.say(number) == result
+    where:
+      number | result | description
+      10     | "Buzz" | "times 5"
+      59     | "Buzz" | "contain 5"
+  }
+
 }
